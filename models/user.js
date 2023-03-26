e19ea3e6d7d14c9c12160198c390cefaf9055a66
Vitaly Chikhalov <ghoston.91@yandex.ru>
2023-03-26 13:26:07 +0300

    #3

--- models/user.js
+++ models/user.js
@@ -8,14 +8,12 @@ const NotAuthError = require('../utils/NotAuthError');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
